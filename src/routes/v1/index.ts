61c41535b88ed3c61efaf43814c1aaaeb9fc3b4d
Alexandre Noblet <alexandrenoblet27@gmail.com>
2026-01-12 15:15:57 +0100

    feat: implement profile routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-// import authRoutes from "./auth.routes";
-// import profileRoutes from "./profile.routes";
+import authRoutes from "./auth.routes";
+import profileRoutes from "./profile.routes";
 // import listingRoutes from "./listing.routes";
 // import bookingRoutes from "./booking.routes";
 // import messageRoutes from "./message.routes";
@@ -12,8 +12,8 @@ router.get("/", (req, res) => {
     res.json({ message: "Welcome to API v1" });
 });
 
-// router.use("/auth", authRoutes);
-// router.use("/profiles", profileRoutes);
+router.use("/auth", authRoutes);
+router.use("/profiles", profileRoutes);
 // router.use("/listings", listingRoutes);
 // router.use("/bookings", bookingRoutes);
 // router.use("/messages", messageRoutes);
